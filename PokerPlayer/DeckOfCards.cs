571fdda8e0dc0604f5ba06d5e3c1475dd39fceb8
Brandy C. Lucero <brandy.c.lucero@gmail.com>
2014-09-17 10:13:25 -0600

    again

---
+++ PokerPlayer/DeckOfCards.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards
+{
+    class PokerPlayer
+    {
+        public List<Card> Hands{get; set;}
+        public PokerPlayer()
+
+        {
+            this.Hands = new List<Card>();
+        }
+        public void DrawCards(List<Card> CardList)
+        {
+            this.Hands = CardList;
+        }
+
+        public bool HasPair()
+        {
+            return this.Hands.Select(x => x.Rank).GroupBy(x => x).Count() == 2;  
+        }
+        public bool HasTwoPair()
+        {
+            return this.Hands.Select(x => x.Rank).Distinct().Count() == 3;
+        }
+        public bool HasThreeOfAKind()
+        {
+            return this.Hands.Select(x => x.Rank).GroupBy(x => x).Count() == 3;
+        }
+        public bool Straight()
+        {
+            return this.Hands.Select(x => x.Rank).GroupBy(x =>x).Count() == 0;
+        }
+        
+    }
+}
---
+++ PokerPlayer/Hands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards
+{
+    class Hands
+    {
+        public enum HandRank
+        {
+            HighCard = 1,
+            OnePair,
+            TwoPair,
+            ThreeOfAKind,
+            Straight,
+            Flush,
+            FullHouse,
+            FourOfAKind,
+            StraightFlush,
+            RoyalFlush
+        }
+    }
+}
